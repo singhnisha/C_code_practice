07a67d247d9826a65e9614a3f94e927aef2337e3
Nisha Singh <Nisha.Singh@wdc.com>
2023-08-03 14:51:31 +0530

    Adding palindrome

--- pattern/hollow_reverse_pyramid.c
+++ pattern/hollow_reverse_pyramid.c
@@ -3,12 +3,25 @@ int main(){
 	int n;
 	scanf("%d",&n);
 	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= n; j++){
+		for(int k = 1; k <= i-1; k++){
+			printf("1");
+		}
+		for(int j = 1; j <= 2*n; j++){
+			//if(j==((n*2) - (2*i)))
+			//	printf("%d ",(n*2)-(2*i));
+			//if(i==1 || i==j || j==((n*2)-(2*i)))
 			if(i==1)
 				printf("*");
-			else
-				print()
+			//else
+			//	printf(" ");
+			if(i==j)
+				printf("2");
+			if(j==n*2-2*i)
+				printf("3");
+			//else
+			//	printf(" ");
 		}
+		printf("\n");
 	}
 return 0;
 }
